e7cda2d3a41ad57abdf9a938cf9dbf6a7fb31a34
Juan Rojo <juanrojochacon@gmail.com>
2020-08-19 14:23:11 +0200

    updated stability plots

--- paper_EELSML/slides/Derivatives.py
+++ paper_EELSML/slides/Derivatives.py
@@ -51,7 +51,7 @@ def smooth(x, window_len, window='hanning'):
     return y[(index-1):-(index)]
 
 
-nrows, ncols = 3,1
+nrows, ncols = 1,1
 gs = matplotlib.gridspec.GridSpec(nrows,ncols)
 plt.figure(figsize=(ncols*9,nrows*7))
 
@@ -65,24 +65,25 @@ for i in range(1):
     ax.set_xlim([0,6])
     ax.tick_params(which='major',direction='in',length=7)
     ax.tick_params(which='minor',length=8)
-    plt.axhline(y=0, color='black', linewidth=1, alpha=.8)
-    plt.axvline(x=0, color='darkgray', linestyle='--', linewidth = 1)
+    plt.axhline(y=0, color='darkgray', linewidth=1.5,linestyle='--')
+    plt.axhline(y=0.9, color='darkgray', linewidth=1.5,linestyle='--')
+    plt.axvline(x=0, color='darkgray', linestyle='--', linewidth = 1.5)
     
         
     for j in ([14]):   
         if i == 0: 
             ax.axhline(y=1, linestyle='--', color='gray')
         
-            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], ratio1, color='lightgray', label='14/17')
-            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], ratio2, color='lightgray', label='14/22')
-            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], ratio3, color='lightgray', label='14/23')
-            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], total_ratio, label='Mean spectrum 14/vacuum')
-            ax.set_title('Derivatives ratio', fontsize=24)
-            ax.set_ylim([-1, 2])
-            ax.set_xlim([1,3.5])   
-            ax.set_ylabel('$dI/dE_{sp14}$ / $dI/dE_{vac}$', fontsize=20)
-            ax.set_xlabel('$\Delta$E (eV)', fontsize=24)
-            ax.legend(fontsize=16)  
+            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], ratio1, color='black', label='Individual',lw=2,ls="dashed")
+            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], ratio2, color='black',ls="dashed",lw=2,)
+            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], ratio3, color='black',ls="dashed",lw=2,)
+            p1 = ax.plot(df_dx['x%(i)s'% {"i": j}], total_ratio, label='Average',lw=3)
+            # ax.set_title('Derivatives ratio', fontsize=24)
+            ax.set_ylim([-0.3, 1.5])
+            ax.set_xlim([1,3.2])   
+            ax.set_ylabel(r'$dI/d \Delta E |_{\rm sample} \,/\,dI/d \Delta E|_{\rm vacuum} $', fontsize=25)
+            ax.set_xlabel(r'$\Delta E$ (eV)', fontsize=24)
+            ax.legend(fontsize=23)  
     
    
     if i == 0:
@@ -92,5 +93,4 @@ for i in range(1):
         ax.tick_params(which='minor', length= 10, labelsize=10)
     
 plt.tight_layout()
-plt.savefig("Derivatives.pdf")
-plt.show()
+plt.savefig("Derivatives_sp4.pdf")
--- paper_EELSML/slides/Stabilityplots.py
+++ paper_EELSML/slides/Stabilityplots.py
@@ -42,10 +42,29 @@ bg_bandgaps = plot_bgvalues.iloc[:, arr == 0].to_numpy()
 bg_high = plot_bgvalues.iloc[:, arr == 1].to_numpy()
 bg_low = plot_bgvalues.iloc[:, arr == 2].to_numpy()
 
+# DeltaE1 array
+dE1_array = [1.60, 1.65, 1.70, 1.75, 1.80, 1.85, 1.9, 1.95, 2.00, 2.05, 2.1, 2.15, 2.2]
+
+# Print values of median and upper and lower 68% CL ranges
+
+print(bg_bandgaps[0])
+l=len(bg_bandgaps[0])
+print(l)
+print("\n *** bandgap values ***")
+for i in range(l):
+    print(dE1_array[i], '%6.2f' % bg_bandgaps[0,i],\
+          '%6.2f' % (bg_high[0,i]-bg_bandgaps[0,i]), \
+          '%6.2f' % (bg_low[0,i]-bg_bandgaps[0,i]) )
+
+l=len(df_bandgaps[0])
+print("\n *** b exponent values ***")
+for i in range(l):
+    print(dE1_array[i], '%6.2f' % df_bandgaps[0,i],\
+          '%6.2f' % (df_high[0,i]-df_bandgaps[0,i]), \
+          '%6.2f' % (df_low[0,i]-df_bandgaps[0,i]) )
+
 ########################################################################
 ########################################################################
-
-
 
 nrows, ncols = 1,2
 gs = matplotlib.gridspec.GridSpec(nrows,ncols)
@@ -61,9 +80,7 @@ label1 = 'Median'
 label2 = '68% CL'
 wl = 4
 
-
-dE1_array = [1.60, 1.65, 1.70, 1.75, 1.80, 1.85, 1.9, 1.95, 2.00, 2.05, 2.1, 2.15, 2.2]
-
+plt.title('Spectrum 14', fontsize=20)
 for i in range(2):
     ax = plt.subplot(gs[i])
    
@@ -73,39 +90,43 @@ for i in range(2):
     ax.tick_params(which='major', direction='in', length= 10, labelsize=14)
     
     if i == 0:
-        ax.set_ylim([0, 3.5])
+        ax.set_ylim([0, 3])
         ax.set_yticks([.5, 1, 1.5, 2, 2.5])
-        ax.set_xlabel('$\Delta$E1 (eV)', fontsize=20)
-        ax.set_ylabel('b', fontsize=20)
+        ax.set_xlabel(r'$\Delta E_{\rm I}$ (eV)', fontsize=22)
+        ax.set_ylabel(r'$b$', fontsize=22)
         
-        ax.plot(dE1_array, smooth(df_bandgaps[i], wl), '--', color='steelblue', label=label1)
-        ax.plot(dE1_array, smooth(df_high[i], wl), '-', color='steelblue',label=label2)
-        ax.plot(dE1_array, smooth(df_low[i], wl), '-', color='steelblue', label=label2)
-        ax.fill_between(dE1_array, smooth(df_high[i], wl), smooth(df_low[i], wl), color='steelblue', alpha=.2, label=label2)
+        ax.plot(dE1_array, smooth(df_bandgaps[0], wl), '--', color='steelblue', label=label1)
+        ax.plot(dE1_array, smooth(df_high[0], wl), '-', color='steelblue',label=label2)
+        ax.plot(dE1_array, smooth(df_low[0], wl), '-', color='steelblue', label=label2)
+        ax.fill_between(dE1_array, smooth(df_high[0], wl), smooth(df_low[0], wl), color='steelblue', alpha=.2, label=label2)
         
     
     if i == 1:
-        ax.set_ylim([0, 3])
-        ax.plot(dE1_array, smooth(bg_bandgaps[i], wl), '--', color='steelblue', label=label1)
-        ax.plot(dE1_array, smooth(bg_high[i], wl), '-', color='steelblue', label = label2)
-        ax.plot(dE1_array, smooth(bg_low[i], wl), '-', color='steelblue', label = label2)
-        ax.fill_between(dE1_array, smooth(bg_high[i], wl), smooth(bg_low[i], wl), \
+        ax.set_ylim([2, 3])
+        ax.plot(dE1_array, smooth(bg_bandgaps[0], wl), '--', color='steelblue', label=label1)
+        ax.plot(dE1_array, smooth(bg_high[0], wl), '-', color='steelblue', label = label2)
+        ax.plot(dE1_array, smooth(bg_low[0], wl), '-', color='steelblue', label = label2)
+        ax.fill_between(dE1_array, smooth(bg_high[0], wl), smooth(bg_low[0], wl), \
                     color='steelblue', alpha=.2, label=label2)
-        ax.set_yticks([0, .5, 1, 1.5, 2])
-        ax.set_ylabel('$E_{bg}$', fontsize=20)
-        ax.set_xlabel('$\Delta$E1 (eV)', fontsize=20)
+        ax.set_yticks([0, .5, 1, 1.25, 1.5, 1.75, 2, 2.25, 2.5])
+        ax.set_ylabel(r'$E_{\rm BG}$ (eV)', fontsize=22)
+        ax.set_xlabel(r'$\Delta E_{\rm I}$ (eV)', fontsize=22)
+        ax.set_ylim([0.95, 2.5])
+        
     
     handles, labels = ax.get_legend_handles_labels()
     ax.legend((handles[0], (handles[1], handles[2], handles[3])), (labels[0], labels[1]), \
-                  loc = 'upper left', fontsize=14)
-    ax.set_title('Spectrum 14', fontsize=15)
-
-    if (i % 2) != 0:
-        ax.set_yticklabels([])
-  
+                  loc = 'upper left', fontsize=17)
     
+
+    #if (i % 2) != 0:
+        # ax.set_yticklabels([])
+  
+
+
 plt.tight_layout()
-plt.savefig('Sability_plots.pdf')
+plt.savefig('Stability_plots_sp14.pdf')
+print("Saved fig = Stability_plots_sp14.pdf")
 
 
 
