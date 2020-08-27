2095910934290ec51034f7471be9d053855d132e
laurienroest <l.i.roest@student.tudelft.nl>
2020-08-27 08:19:30 +0200

    removed unnecessairy functions

--- Functions.py
+++ Functions.py
@@ -107,41 +107,8 @@ def get_mean_pseudodata(x,y, nbins):
     stdvector = vectorize_variance(x,y,nbins)
     return mean, std, meanvector, stdvector
 
-def plot_uncertainties(x, y, nbins, minval, maxval):
-    
-    nbins
-    df_train, cuts1, cuts2 = ewd(x, y, nbins)
-    length = len(df_train[:,0])
-    mean, std, count = binned_statistics(x,y,nbins)
 
-    ## Plot the output
-    plt.errorbar(np.linspace(minval, maxval, nbins), mean, yerr = std, linestyle=':', marker='.', ecolor='cadetblue', mfc='steelblue', mec='darkslategray', label='Median and std')
-    plt.legend(loc='upper left')
-    plt.ylim([7,17.5])
-    
-def plot_relative_error(x, y, nbins, minval, maxval):
-    df_train, cuts1, cuts2 = ewd(x, y)
-    length = len(df_train[:,0])
-    mean, std, count = binned_statistics(x,y)
-    
-    rel_error = np.divide(std, mean)
-    ax2 = plt.twinx()
-    
-    ax2.plot(np.linspace(minval, maxval, nbins), rel_error, 'x', color='lightgray',  label='Relative error')
-    plt.ylim([0,0.5])
-    
-
-def plot_smoothing(x,y,nbins,minval,maxval):
-    nbins
-    df_train, cuts1, cuts2 = ewd(x, y)
-    length = len(df_train[:,0])
-    mean, std, count = binned_statistics(x,y)
-
-    ## Plot the output
-    plt.errorbar(np.linspace(minval, maxval, nbins), mean, yerr = std, linestyle=':', marker='.',
-                  mfc='red')
-    plt.plot(np.linspace(minval, maxval, length))
-    plt.title('Binned means and std')
+   
     
 """Neural network functions: """
     
@@ -210,99 +177,13 @@ def window(x,y, minval, maxval):
     y = y[treshold_min:treshold_max]
     
     return x,y
-
-def select_training_data(time):
     
-    x_train, y_train, x_val, y_val = prepare_mix_data(time)
-    df_train, cuts1_t, cuts2_t = ewd(x_train, y_train)
-    mean_t, std_t, count_t= binned_statistics(x_train, y_train)
-    variance_vector_t = vectorize_variance(x_train, y_train)
-    mean_vector_t = vectorize_mean(x_train, y_train)
-    
-    #create one vector with (x_train, y_train, sigma_train)
-    df_train = np.c_[df_train, variance_vector_t]
-    
-def select_validation_data(time):
-    
-    x_train, y_train, x_val, y_val = prepare_mix_data(time)
-    df_val, cuts1_v, cuts2_v = ewd(x_val, y_val)
-    mean_v, std_v, count_v= binned_statistics(x_val, y_val)
-    variance_vector_v = vectorize_variance(x_val, y_val)
-    mean_vector_v = vectorize_mean(x_val, y_val)
-    #create one vector with (x_train, y_train, sigma_train)
-    df_val = np.c_[df_val, variance_vector_v]
-    
-    
-def add_time_energy(x, ms, energy):
-    """Function that adds second input dimension for exposure time"""
-    time = np.array(ms)
-    energy = np.array(energy)
-    N_train = len(x[:,0])
-
-    train_x_a = copy(x[:,0]).reshape(N_train,1)  ## Column 1 = Eloss, colum 2 = time (binary)
-    train_x_time = train_x_a*0 + int(time)
-    train_x_energy = train_x_a*0 + int(energy)
-    train_x = np.c_[copy(x[:,0]), train_x_time, train_x_energy] 
-    return train_x
 
 def residuals(prediction, y, std):
     res = np.divide((prediction - y), std)
     return res
 
-from scipy import stats
 
-
-
-def confidence_level_1(x, y, nbins, level):
-    
-    df_train, cuts1, cuts2 = ewd(x, y, nbins)
-
-    
-    def CI_mean(data, confidence=0.95):
-        a = 1.0 * np.array(data)
-        n = len(a)
-        m = np.mean(a)
-        
-        return m
-    
-    def CI_high(data, confidence=0.95):
-        ## remove the lowest and highest 16% of the values
-        
-        a = 1.0 * np.array(data)
-        n = len(a)
-        b = np.sort(data)
-
-        highest = np.int((1-(1-confidence)/2)*len(a))
-        high_a = b[highest]
- 
-        return high_a
-    
-    def CI_low(data, confidence=0.95):
-        ## remove the lowest and highest 16% of the values
-        
-        a = 1.0 * np.array(data)
-        n = len(a)
-        b = np.sort(data)
-        lowest = np.int(((1-confidence)/2)*len(a))
-        low_a = b[lowest]
-
-        return low_a
-
-
-    CI_mean, edges, binnum = scipy.stats.binned_statistic(df_train[:,0], df_train[:,1], statistic=CI_mean, bins=cuts2)
-    CI_high, edges, binnum = scipy.stats.binned_statistic(df_train[:,0], df_train[:,1], statistic=CI_high, bins=cuts2)
-    CI_low, edges, binnum = scipy.stats.binned_statistic(df_train[:,0], df_train[:,1], statistic=CI_low, bins=cuts2)
-    
-    return CI_mean, CI_high, CI_low
-
-
-
-
-    
-    
-
-
-# In[ ]:
 
 
 
