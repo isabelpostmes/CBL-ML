55b0a864ea7ceeb7349f7b6c9f16ee1b42e46ce6
laurienroest <l.i.roest@student.tudelft.nl>
2020-08-31 09:32:59 +0200

    updated predictions with bigger energy cut

--- paper_EELSML/sourceplots/prediction_with_cuts_10ms.py
+++ paper_EELSML/sourceplots/prediction_with_cuts_10ms.py
@@ -20,22 +20,32 @@ import matplotlib
 
 energy_file_full = pd.read_csv('data/prediction_120keV_full')
 energy_file_cut = pd.read_csv('data/prediction_120keV_cut')
+energy_file_cut_big = pd.read_csv('data/Energy_extrapolation_cut_big')
 
 
 hfont = rc('font',**{'family':'sans-serif','sans-serif':['Sans Serif']})
 
 groups_full = energy_file_full.groupby(['time', 'energy'])
 groups_cut = energy_file_cut.groupby(['time', 'energy'])
+groups_cut_big  = energy_file_cut_big.groupby(['time', 'energy'])
 ncols, nrows = 1,1
 
 gs = matplotlib.gridspec.GridSpec(nrows,ncols)
-plt.figure(figsize=(ncols*5,nrows*3.5))
+plt.figure(figsize=(ncols*6,nrows*4))
 ax = plt.subplot(gs[0])
 
 
 rescolors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 
-
+for name, group in groups_cut_big:
+    mean_prediction = group.iloc[:, 4:].mean(axis=1).to_numpy()
+    std_prediction = group.iloc[:, 4:].std(axis=1).to_numpy()
+    
+    if group['time'].max() == .1 and group['energy'].max() == 2:
+        p3=ax.plot(group.x*1000, np.divide(mean_prediction, mean_prediction), label='with data cut at 50meV',color='gray',ls="dotted")
+        p3a=ax.fill_between(group.x*1000, np.divide(mean_prediction + std_prediction, mean_prediction), \
+                        np.divide(mean_prediction - std_prediction, mean_prediction), alpha=.3,color='lightgray')     
+        
 for name, group in groups_full:
     mean_prediction = group.iloc[:, 4:].mean(axis=1).to_numpy()
     std_prediction = group.iloc[:, 4:].std(axis=1).to_numpy()
@@ -51,12 +61,11 @@ for name, group in groups_cut:
     std_prediction = group.iloc[:, 4:].std(axis=1).to_numpy()
     
     if group['time'].max() == .1 and group['energy'].max() == 2:
-        p2=ax.plot(group.x*1000, np.divide(mean_prediction, mean_prediction), label='with data cut',color=rescolors[0],ls="dashed")
+        p2=ax.plot(group.x*1000, np.divide(mean_prediction, mean_prediction), label='with data cut at 100meV',color=rescolors[0],ls="dashed")
         p2a=ax.fill_between(group.x*1000, np.divide(mean_prediction + std_prediction, mean_prediction), \
                         np.divide(mean_prediction - std_prediction, mean_prediction), alpha=.3,color=rescolors[0])
         
-      
-        
+ax.axvline(x=50, linestyle='dashdot', color='black') 
 ax.axvline(x=100, linestyle='dashdot', color='black') 
 ax.axvline(x=800, linestyle='dashdot', color='black') 
 ax.set_title('$t_{exp}$ = 10 ms, $E_b$ = 200 keV', fontsize = 15)
@@ -69,7 +78,7 @@ ax.tick_params(which='major',direction='in',length=5, labelsize=12)
 ax.tick_params(which='minor',direction='in',length=5, labelsize=12)
 
 
-ax.legend([(p1[0],p1a),(p2[0],p2a)],["no data cut","with data cut"], frameon=True,loc=[0.53,0.74],prop={'size':11})
+ax.legend([(p1[0],p1a),(p2[0],p2a),(p3[0],p3a) ],["no data cut","with data cut at 100meV","with data cut at 50meV"], frameon=True,loc=[0.53,0.74],prop={'size':11})
 
 
 plt.tight_layout()
