9e4ad8b73677fac68fc92c601d343ac3c8b4e10c
Juan Rojo <juanrojochacon@gmail.com>
2020-09-02 13:48:45 +0200

    work on sect 5

--- paper_EELSML/sourceplots/subtracted_spectra_1p45eV.py
+++ paper_EELSML/sourceplots/subtracted_spectra_1p45eV.py
@@ -31,9 +31,9 @@ def smooth(x, window_len, window='hanning'):
 
 ##################################################################
 
-nrows, ncols = 1,1
+nrows, ncols = 1,2
 gs = matplotlib.gridspec.GridSpec(nrows,ncols)
-plt.figure(figsize=(ncols*9,nrows*7))
+plt.figure(figsize=(ncols*5,nrows*3.5))
 
 rescolors = plt.rcParams['axes.prop_cycle'].by_key()['color']
 hfont = rc('font',**{'family':'sans-serif','sans-serif':['Sans Serif']})
@@ -43,29 +43,54 @@ lines = ['solid', 'dotted', 'dashed', 'dashdot']
 nbins = 1861
 wl = 4
           
-for i in ([4,5,6,7]):
+#for i in ([4,5,6,7]):
+for i in ([4,5,6]):
     j = i - 4
     ax = plt.subplot(gs[0])
     ax.set_xlim([.8,2.5])
     ax.set_ylim([-1e1,1.5e3])
-    ax.set_ylabel('Intensity (a.u.)', fontsize=26)
-    ax.set_xlabel('Energy loss (eV)', fontsize=26)
+    ax.set_ylabel('Intensity (a.u.)', fontsize=18)
+    ax.set_xlabel('Energy loss (eV)', fontsize=17)
     
-    ax.tick_params(which='major',direction='in',length=10, labelsize=16)
+    ax.tick_params(which='major',direction='in',length=10, labelsize=13)
     ax.tick_params(which='minor',length=10)
     ax.set_xticks([1, 1.5, 2, 2.5])
     label= '%(i)s'%{"i":str(i)}
     
-    p1 = ax.plot(np.linspace(-.3, 9, nbins), smooth(mean_rep['dif%(i)s_median' % {"i": i}], wl), linestyle=lines[j],color=rescolors[j],lw=2, label= 'Sp %(i)s'%{"i":i})
+    p1 = ax.plot(np.linspace(-.3, 9, nbins), smooth(mean_rep['dif%(i)s_median' % {"i": i}], wl), linestyle=lines[j],color=rescolors[j],lw=2, label= 'sp %(i)s'%{"i":i})
     ax.fill_between(np.linspace(-.3, 9, nbins), smooth(mean_rep['dif%(i)s_low'% {"i": i}], wl), smooth(mean_rep['dif%(i)s_high'% {"i": i}], wl), color=rescolors[j], alpha=.2)
     p1b =ax.fill(np.NaN,np.NaN,color=rescolors[j],alpha=0.2)
     ax.set_yticks([])
-    ax.legend(loc='upper left', fontsize=16)
+    ax.legend(loc='upper left', fontsize=15)
+    plt.text(1.7,250,r"$\Delta E_{\rm I}=1.45~{\rm eV}$",fontsize=15)
+
+
+mean_rep = pd.read_csv(fnamebase + 'Subtracted_spectra_1.55.csv')
+
+for i in ([4,5,6]):
+    j = i - 4
+    ax = plt.subplot(gs[1])
+    ax.set_xlim([.8,2.5])
+    ax.set_ylim([-1e1,1.5e3])
+    # ax.set_ylabel('Intensity (a.u.)', fontsize=17)
+    ax.set_xlabel('Energy loss (eV)', fontsize=17)
+    
+    ax.tick_params(which='major',direction='in',length=10, labelsize=13)
+    ax.tick_params(which='minor',length=10)
+    ax.set_xticks([1, 1.5, 2, 2.5])
+    label= '%(i)s'%{"i":str(i)}
+    
+    p1 = ax.plot(np.linspace(-.3, 9, nbins), smooth(mean_rep['dif%(i)s_median' % {"i": i}], wl), linestyle=lines[j],color=rescolors[j],lw=2, label= 'sp %(i)s'%{"i":i})
+    ax.fill_between(np.linspace(-.3, 9, nbins), smooth(mean_rep['dif%(i)s_low'% {"i": i}], wl), smooth(mean_rep['dif%(i)s_high'% {"i": i}], wl), color=rescolors[j], alpha=.2)
+    p1b =ax.fill(np.NaN,np.NaN,color=rescolors[j],alpha=0.2)
+    ax.set_yticks([])
+    ax.legend(loc='upper left', fontsize=15)
+    plt.text(1.7,250,r"$\Delta E_{\rm I}=1.55~{\rm eV}$",fontsize=15)
 
     
 plt.tight_layout()
-plt.savefig('SubtractedEELS_plot_1.45eV.pdf')
-print("Saved fig = SubtractedEELS_plot_1.45eV.pdf")
+plt.savefig('../plots/subtracted_spectra_comp.pdf')
+print("Saved fig = ../plots/subtracted_spectra_comp.pdf")
 
 
 
