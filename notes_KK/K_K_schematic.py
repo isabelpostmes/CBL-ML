492d4ce3b00272c2455372ccacae0ca0e5899ec5
isabelpostmes <isabelpostmes@gmail.com>
2020-09-03 23:56:28 +0200

    small adds

--- notes_KK/K_K_schematic.py
+++ notes_KK/K_K_schematic.py
@@ -32,7 +32,7 @@ gamma = (1-v**2/c**2)**-0.5
 theta_E = E/(gamma*m_0*v**2)
 log_term = np.log(1+(beta/theta_E)**2)
 
-J_ac = J1_E/theta_E
+J_ac = J1_E/log_term
 
 
 #step 2: extrapolation
@@ -55,8 +55,20 @@ J_extrp[shape_J:] = A*np.power(E_extrp[shape_J:],r)
 #step 3: normalisation
 
 Re_eps0 = 0 #value of Re[1/eps(0)]
-int_J_over_E = np.sum(J_extrp/E_extrp)
+int_J_over_E = np.sum(J_extrp/E_extrp)*dE
 K = 2*int_J_over_E/(math.pi*(1-Re_eps0))
+
+
+#step 4: retreiving ε
+Im_eps = J_extrp/K #Im[-1/eps(E)]
+
+Re_eps = np.zeros(sem_inf) # Re[1/eps(0)]
+
+#integrate around each energy (to avoid singularities)
+for i in range(E_extrp.size):
+    E = E_extrp[i]
+    select = (E_extrp != E)
+    Re_eps[i] = 1 - 2/math.pi * np.sum(Im_eps[select]*E_extrp[select]/(np.power(E_extrp[select],2)-E**2))
 
 
 
