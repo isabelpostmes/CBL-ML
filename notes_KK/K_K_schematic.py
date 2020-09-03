3a20d8d6490652cd201a8cff029d61296e6ddce7
isabelpostmes <isabelpostmes@gmail.com>
2020-09-03 17:35:54 +0200

    added normalisation

--- notes_KK/K_K_schematic.py
+++ notes_KK/K_K_schematic.py
@@ -15,7 +15,7 @@ import math
 shape_J = 100
 
 J1_E = np.ones(shape_J)
-E = np.linspace(0,shape_J-1, shape_J)
+E = np.linspace(1,shape_J, shape_J)
 
 
 #step 1: modulate intensity
@@ -32,26 +32,36 @@ gamma = (1-v**2/c**2)**-0.5
 theta_E = E/(gamma*m_0*v**2)
 log_term = np.log(1+(beta/theta_E)**2)
 
-I_ac = J1_E/theta_E
+J_ac = J1_E/theta_E
 
 
 #step 2: extrapolation
 r = 3 #Drude model, can also use estimation from exp. data
-A = I_ac[-1]
+A = J_ac[-1]
 n_times_extra = 10
 sem_inf = shape_J*(n_times_extra+1)
 
 dE = (E[-1]-E[0])/E.size
 
-I_extrp = np.zeros(sem_inf)
+J_extrp = np.zeros(sem_inf)
 E_extrp = np.linspace(E[0], sem_inf*dE+dE, sem_inf)
 
-I_extrp[:shape_J] = I_ac
+J_extrp[:shape_J] = J_ac
 E_extrp[:shape_J] = E
 
-I_extrp[shape_J:] = A*np.power(E_extrp[shape_J:],r)
+J_extrp[shape_J:] = A*np.power(E_extrp[shape_J:],r)
 
 
 #step 3: normalisation
+
+Re_eps0 = 0 #value of Re[1/eps(0)]
+int_J_over_E = np.sum(J_extrp/E_extrp)
+K = 2*int_J_over_E/(math.pi*(1-Re_eps0))
+
+
+
+
+
+
 
 
